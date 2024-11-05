902f00fbafb167d5d1ae6d196c77dd8d55cd2daf
staskachaev <staskachaev205@mail.ru>
2024-11-05 17:12:50 +0700

    display list and detail informations

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 # Create your views here.
 
@@ -21,3 +22,30 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+    # def get_context_data(self, **kwargs):
+    #     # В первую очередь получаем базовую реализацию контекста
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Добавляем новую переменную к контексту и инициализируем её некоторым значением
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+    #
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5]
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+
